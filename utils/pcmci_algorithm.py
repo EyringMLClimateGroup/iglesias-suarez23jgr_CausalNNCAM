163d502e61c1a8b3f0316cb63c8764a62cc7dcb0
Breixo Soliño <Breixo.SolinoFernandez@dlr.de>
2021-03-23 15:38:14 +0100

    Reformatted with Black, editted back commented code lines.

--- utils/pcmci_algorithm.py
+++ utils/pcmci_algorithm.py
@@ -1,9 +1,9 @@
-import numpy                          as np
-import utils.links                    as links
-from utils.constants              import TAU_MIN, TAU_MAX
+import numpy as np
+import utils.links as links
+from utils.constants import TAU_MIN, TAU_MAX
 import tigramite
 from tigramite import data_processing as pp
-from tigramite.pcmci              import PCMCI
+from tigramite.pcmci import PCMCI
 
 
 def run_pc_stable(pcmci, selected_links, list_pc_alpha):
@@ -11,10 +11,10 @@ def run_pc_stable(pcmci, selected_links, list_pc_alpha):
     for pc_alpha in list_pc_alpha:
         try:
             links = pcmci.run_pc_stable(
-                    tau_min = TAU_MIN,
-                    tau_max = TAU_MAX,
-                    selected_links = selected_links,
-                    pc_alpha = pc_alpha
+                tau_min=TAU_MIN,
+                tau_max=TAU_MAX,
+                selected_links=selected_links,
+                pc_alpha=pc_alpha,
             )
             # "result_parents" will now be a dictionary with only the
             # causal parents for each variable like selected_links, but
@@ -27,17 +27,17 @@ def run_pc_stable(pcmci, selected_links, list_pc_alpha):
 #             )['link_matrix']
 
             results = {
-                "links"       : links,
-                "p_matrix"    : pcmci.p_matrix,
-                "val_matrix"  : pcmci.val_matrix,
+                "links": links,
+                "p_matrix": pcmci.p_matrix,
+                "val_matrix": pcmci.val_matrix,
 #                 "link_matrix" : link_matrix,
-                "var_names"   : pcmci.var_names
+                "var_names": pcmci.var_names,
             }
         except ValueError as e:
             print(e)
             results = {}
         pc_alpha_results[str(pc_alpha)] = results
-        
+
     return pc_alpha_results
 
 
@@ -51,11 +51,11 @@ def run_pc_stable_2(pcmci, list_pc_alpha):
         results_parents = dict()
         for j in children:
             results_parents[j] = pcmci_list.run_pc_stable_single(
-                    j,
-                    tau_min = TAU_MIN,
-                    tau_max = TAU_MAX,
-                    selected_links = selected_links[j],
-                    pc_alpha = pc_alpha
+                j,
+                tau_min=TAU_MIN,
+                tau_max=TAU_MAX,
+                selected_links=selected_links[j],
+                pc_alpha=pc_alpha,
             )
         pc_alpha_results[str(pc_alpha)] = results_parents
 
@@ -68,32 +68,27 @@ def run_pc_stable_3(pcmci, list_pc_alpha):
     results_parents = dict()
     for j in children:
         results_parents[j] = pcmci.run_pc_stable_single(
-                j,
-                tau_min = TAU_MIN,
-                tau_max = TAU_MAX,
-                selected_links = selected_links[j],
-                pc_alpha = list_pc_alpha
+            j,
+            tau_min=TAU_MIN,
+            tau_max=TAU_MAX,
+            selected_links=selected_links[j],
+            pc_alpha=list_pc_alpha,
         )
-    # For each parent you may get a different pc_alpha        
-        
+    # For each parent you may get a different pc_alpha
 
-def find_links(list_var_data, list_pc_alpha, cond_ind_test, verbosity = 0):
+
+def find_links(list_var_data, list_pc_alpha, cond_ind_test, verbosity=0):
     spcam_data, var_names, parents, children = links.prepare_tigramite_data(
-            list_var_data)
-    
+        list_var_data
+    )
+
     selected_links = links.select_links(TAU_MIN, TAU_MAX, parents, children)
-    
+
     # Initialize dataframe object, specify time axis and variable names
     dataframe = pp.DataFrame(
-            spcam_data, 
-            datatime = np.arange(len(spcam_data)),
-            var_names= var_names
+        spcam_data, datatime=np.arange(len(spcam_data)), var_names=var_names
     )
 
-    pcmci = PCMCI(
-            dataframe=dataframe, 
-            cond_ind_test=cond_ind_test,
-            verbosity = verbosity
-    )
-    
+    pcmci = PCMCI(dataframe=dataframe, cond_ind_test=cond_ind_test, verbosity=verbosity)
+
     return run_pc_stable(pcmci, selected_links, list_pc_alpha)
--- utils/utils.py
+++ utils/utils.py
@@ -1,20 +1,21 @@
-import numpy    as np
+import numpy as np
 import numpy.ma as ma
-from pathlib    import Path
+from pathlib import Path
 from .constants import DATA_FOLDER, ANCIL_FILE, FILENAME_PATTERN, SPCAM_Vars
-from netCDF4    import Dataset
+from netCDF4 import Dataset
 import pickle
 
 #########################
 #    Find region utils
 #########################
 
+
 def find_closest_value(values, target):
     """
     Returns the index of the closest value to the target
     in an array of values. Assumes values are numerical.
     """
-    
+
     differences = np.array(values) - target
     idx = abs(differences).argmin()
     return idx
@@ -22,8 +23,8 @@ def find_closest_value(values, target):
 
 def find_closest_longitude(longitudes, target):
     """Converts longitudes to 0-360 and returns the index to the closest"""
-    
-    target = (target+360)%360
+
+    target = (target + 360) % 360
     return find_closest_value(longitudes, target)
 
 
@@ -31,19 +32,19 @@ def get_gridpoints(region):
     gridpoints = []
     levels, latitudes, longitudes = read_ancilaries(Path(DATA_FOLDER, ANCIL_FILE))
     idx_lats = [find_closest_value(latitudes, lat) for lat in region[0]]
-    lats_lim = latitudes[idx_lats[0]:idx_lats[-1]+1]
+    lats_lim = latitudes[idx_lats[0] : idx_lats[-1] + 1]
     for iLat in lats_lim:
         idx_lons = [find_closest_longitude(longitudes, lon) for lon in region[1]]
-        lons_lim = longitudes[idx_lons[0]:idx_lons[-1]+1]
+        lons_lim = longitudes[idx_lons[0] : idx_lons[-1] + 1]
         for iLon in lons_lim:
-            gridpoints.append([iLat,iLon]) 
+            gridpoints.append([iLat, iLon])
     return gridpoints
 
 
 def get_levels(pres):
     levels, latitudes, longitudes = read_ancilaries(Path(DATA_FOLDER, ANCIL_FILE))
     idx_levs = [find_closest_value(levels, lev) for lev in pres]
-    levs_lim = levels[idx_levs[-1]:idx_levs[0]+1]
+    levs_lim = levels[idx_levs[-1] : idx_levs[0] + 1]
     return levs_lim
 
 
@@ -51,10 +52,10 @@ def get_levels(pres):
 #    Load ancil utils
 #########################
 def read_ancilaries(path):
-    with Dataset(path, 'r') as nc_ancil:
-        levels     = nc_ancil.variables['lev'][:]
-        latitudes  = nc_ancil.variables['lat'][:]
-        longitudes = nc_ancil.variables['lon'][:]
+    with Dataset(path, "r") as nc_ancil:
+        levels = nc_ancil.variables["lev"][:]
+        latitudes = nc_ancil.variables["lat"][:]
+        longitudes = nc_ancil.variables["lon"][:]
     return levels, latitudes, longitudes
 
 
@@ -65,8 +66,8 @@ class VarData:
     """
     Helper class to store the variable data and metadata
     """
-    
-    def __init__(self, variable, data, level = None):
+
+    def __init__(self, variable, data, level=None):
         self.variable = variable
         self.level = level
         self.data = data
@@ -77,7 +78,7 @@ class VarData:
 
 
 def read_spcam_file(path, var_name):
-    with Dataset(path, 'r') as file:
+    with Dataset(path, "r") as file:
         data = file.variables[var_name][:]
     return data
 
@@ -86,9 +87,10 @@ def normalize(values):
     anom = values - np.mean(values)
     std = np.std(anom, ddof=1)
     if std != 0:
-        return anom/std
+        return anom / std
     else:
         return values
+
 
 def log_normalize(values):
     values = ma.log(values)
@@ -96,29 +98,31 @@ def log_normalize(values):
     anom = values - np.mean(values)
     std = np.std(anom, ddof=1)
     if std != 0:
-        return anom/std
+        return anom / std
     else:
 #         print(f"Values for prect are zero; check data. Stop processing!")
 #         exit()
         raise ValueError("Values for prect are zero; check data.")
 
 
-def get_normalized_data(
-        variable, experiment, folder, idx_lats, idx_lons, level):
+def get_normalized_data(variable, experiment, folder, idx_lats, idx_lons, level):
     """
     Returns normalized data for one level
     """
-    filename = Path(folder, FILENAME_PATTERN.format(
-            var_name   = variable.name,
-            level      = [1,level+1][variable.dimensions==3],
-            experiment = experiment
-    ))
+    filename = Path(
+        folder,
+        FILENAME_PATTERN.format(
+            var_name=variable.name,
+            level=[1, level + 1][variable.dimensions == 3],
+            experiment=experiment,
+        ),
+    )
     data = read_spcam_file(filename, variable.name)
-    
-    if variable.dimensions == 3:        
-        level_data = data[:,0,idx_lats,idx_lons]
+
+    if variable.dimensions == 3:
+        level_data = data[:, 0, idx_lats, idx_lons]
     elif variable.dimensions == 2:
-        level_data = data[:,idx_lats,idx_lons]
+        level_data = data[:, idx_lats, idx_lons]
 
     if variable == SPCAM_Vars.prect:
         print(f"Log normalization...")
@@ -126,26 +130,22 @@ def get_normalized_data(
     else:
         return normalize(level_data)
 
-    
+
 def load_data(var_list, experiment, folder, idx_lvls, idx_lats, idx_lons):
     data = list()
     for var in var_list:
         for target_lvl, idx_lvl in idx_lvls:
             norm_data = get_normalized_data(
-                var, 
-                experiment, 
-                folder, 
-                idx_lats, 
-                idx_lons, 
-                idx_lvl)
+                var, experiment, folder, idx_lats, idx_lons, idx_lvl
+            )
             if var.dimensions == 3:
                 var_data = VarData(var, norm_data, target_lvl)
             elif var.dimensions == 2:
                 var_data = VarData(var, norm_data)
             data.append(var_data)
-            
-            if var.dimensions == 2:     
-                break # Stop loading data after the first level
+
+            if var.dimensions == 2:
+                break  # Stop loading data after the first level
     return data
 
 
@@ -154,20 +154,16 @@ def load_data_concat(var_list, experiment, folder, idx_lvls, idx_lats, idx_lons)
     for var in var_list:
         for target_lvl, idx_lvl in idx_lvls:
             norm_data = get_normalized_data(
-                var, 
-                experiment, 
-                folder, 
-                idx_lats, 
-                idx_lons, 
-                idx_lvl)
+                var, experiment, folder, idx_lats, idx_lons, idx_lvl
+            )
             data.append(norm_data)
-            if var.dimensions == 2:     
-                break # Stop loading data after the first level
+            if var.dimensions == 2:
+                break  # Stop loading data after the first level
     return np.array(data)
 
 
 def format_data(norm_data, var_list, idx_lvls):
-    data  = list()
+    data = list()
     count = 0
     for var in var_list:
         for target_lvl, idx_lvl in idx_lvls:
@@ -176,10 +172,10 @@ def format_data(norm_data, var_list, idx_lvls):
             elif var.dimensions == 2:
                 var_data = VarData(var, norm_data[count])
             data.append(var_data)
-            
-            if var.dimensions == 2:     
-                break # Stop loading data after the first level
-            count+=1
+
+            if var.dimensions == 2:
+                break  # Stop loading data after the first level
+            count += 1
     return data
 
 
@@ -187,29 +183,31 @@ def format_data(norm_data, var_list, idx_lvls):
 #    Save data utils
 #########################
 def generate_results_filename_single(
-        var, level, lat, lon, ind_test, experiment, pattern, folder):
+    var, level, lat, lon, ind_test, experiment, pattern, folder
+):
     results_filename = pattern.format(
-            var_name = var.name,
-            level = level+1,
-            lat = int(lat),
-            lon = int(lon),
-            ind_test = ind_test,
-            experiment = experiment
+        var_name=var.name,
+        level=level + 1,
+        lat=int(lat),
+        lon=int(lon),
+        ind_test=ind_test,
+        experiment=experiment,
     )
     return Path(folder, results_filename)
 
 
 def generate_results_filename_concat(
-        var, level, gridpoints, ind_test, experiment, pattern, folder):
+    var, level, gridpoints, ind_test, experiment, pattern, folder
+):
     results_filename = pattern.format(
-            var_name   = var.name,
-            level      = level+1,
-            lat1       = int(gridpoints[0][0]),
-            lat2       = int(gridpoints[-1][0]),
-            lon1       = int(gridpoints[0][-1]),
-            lon2       = int(gridpoints[-1][-1]),
-            ind_test = ind_test,
-            experiment = experiment
+        var_name=var.name,
+        level=level + 1,
+        lat1=int(gridpoints[0][0]),
+        lat2=int(gridpoints[-1][0]),
+        lon1=int(gridpoints[0][-1]),
+        lon2=int(gridpoints[-1][-1]),
+        ind_test=ind_test,
+        experiment=experiment,
     )
     return Path(folder, results_filename)
 
@@ -218,7 +216,7 @@ def save_results(results, file):
     Path(file).parents[0].mkdir(parents=True, exist_ok=True)
     with open(file, "wb") as f:
         pickle.dump(results, f)
-    print(f"Saved results into \"{file}\"")
+    print(f'Saved results into "{file}"')
 
 
 def load_results(file):
