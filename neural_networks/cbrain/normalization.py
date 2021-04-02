cbcea1f8e915c845c04cf85fd81a38380998059e
Breixo Soliño <Breixo.SolinoFernandez@dlr.de>
2021-04-02 13:22:02 +0200

    Updated to normalize only the data in var_dict

--- neural_networks/cbrain/normalization.py
+++ neural_networks/cbrain/normalization.py
@@ -53,15 +53,13 @@ class Normalizer(object):
 class InputNormalizer(object):
     """Normalizer that subtracts and then divides."""
 
-    def __init__(
-        self, norm_ds, var_list, sub="mean", div="std_by_var", var_cut_off=None
-    ):
-        var_idxs = return_var_idxs(norm_ds, var_list, var_cut_off)
+    def __init__(self, norm_ds, var_dict, sub="mean", div="std_by_var"):
+        var_idxs = return_var_idxs(norm_ds, var_dict)
         self.sub = norm_ds[sub].values[var_idxs]
         if div == "maxrs":
             rang = norm_ds["max"][var_idxs] - norm_ds["min"][var_idxs]
             std_by_var = rang.copy()
-            for v in var_list:
+            for v in var_dict.keys():
                 std_by_var[std_by_var.var_names == v] = norm_ds["std_by_var"][
                     norm_ds.var_names_single == v
                 ]
@@ -70,7 +68,7 @@ class InputNormalizer(object):
             # SR: Total mess. Should be handled better
             tmp_var_names = norm_ds.var_names[var_idxs]
             self.div = np.zeros(len(tmp_var_names))
-            for v in var_list:
+            for v in var_dict.keys():
                 std_by_var = norm_ds["std_by_var"][norm_ds.var_names_single == v]
                 self.div[tmp_var_names == v] = std_by_var
         else:
@@ -87,13 +85,13 @@ class InputNormalizer(object):
 class DictNormalizer(object):
     """Normalizer that takes a conversion dictionary as input. Simply scales by factors in dict."""
 
-    def __init__(self, norm_ds, var_list, dic=None):
+    def __init__(self, norm_ds, var_dict, dic=None):
         if dic is None:
             dic = conversion_dict
-        var_idxs = return_var_idxs(norm_ds, var_list)
+        var_idxs = return_var_idxs(norm_ds, var_dict)
         var_names = norm_ds.var_names[var_idxs].copy()
         scale = []
-        for v in var_list:
+        for v in var_dict.keys():
             s = np.atleast_1d(dic[v])
             ns = len(s)
             nv = np.sum(var_names == v)
