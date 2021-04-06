f9fb158c35c0fa34bf41459a40c6290de8e34b19
Breixo Soliño <Breixo.SolinoFernandez@dlr.de>
2021-04-06 19:24:36 +0200

    Moved initialization to __enter__, solved bug with __enter__

--- neural_networks/cbrain/data_generator.py
+++ neural_networks/cbrain/data_generator.py
@@ -39,53 +39,9 @@ class DataGenerator(tf.keras.utils.Sequence):
         self.input_vars = list(input_vars_dict.keys())
         self.output_vars = list(output_vars_dict.keys())
         self.batch_size, self.shuffle = batch_size, shuffle
-
-        # Open datasets
-        self.data_ds = xr.open_dataset(data_fn)
-        if norm_fn is not None:
-            self.norm_ds = xr.open_dataset(norm_fn)
-
-        # Compute number of samples and batches
-        self.n_samples = self.data_ds.vars.shape[0]
-        self.n_batches = int(np.floor(self.n_samples) / self.batch_size)
-
-        # Get input and output variable indices
-        self.input_idxs = return_var_idxs(self.data_ds, input_vars_dict)
-        self.output_idxs = return_var_idxs(self.data_ds, output_vars_dict)
-        self.n_inputs, self.n_outputs = len(self.input_idxs), len(self.output_idxs)
-
-        # Initialize input and output normalizers/transformers
-        if input_transform is None:
-            self.input_transform = Normalizer()
-        elif type(input_transform) is tuple:
-            self.input_transform = InputNormalizer(
-                self.norm_ds,
-                self.input_vars_dict,
-                input_transform[0],
-                input_transform[1],
-                #                 var_cut_off,
-            )
-        else:
-            self.input_transform = (
-                input_transform  # Assume an initialized normalizer is passed
-            )
-
-        if output_transform is None:
-            self.output_transform = Normalizer()
-        elif type(output_transform) is dict:
-            self.output_transform = DictNormalizer(
-                self.norm_ds, self.output_vars_dict, output_transform
-            )
-        else:
-            self.output_transform = (
-                output_transform  # Assume an initialized normalizer is passed
-            )
-
-        # Now close the xarray file and load it as an h5 file instead
-        # This significantly speeds up the reading of the data...
-        if not xarray:
-            self.data_ds.close()
-            self.data_ds = h5py.File(data_fn, "r")
+        self.input_transform = input_transform
+        self.output_transform = output_transform
+        self.xarray = xarray
 
     def __len__(self):
         return self.n_batches
@@ -114,8 +70,61 @@ class DataGenerator(tf.keras.utils.Sequence):
             np.random.shuffle(self.indices)
 
     def __enter__(self):
-        # TODO Move code to retrieve data here
-        pass
+        # Open datasets
+        self.data_ds = xr.open_dataset(self.data_fn)
+        if self.norm_fn is not None:
+            self.norm_ds = xr.open_dataset(self.norm_fn)
+
+        # Compute number of samples and batches
+        self.n_samples = self.data_ds.vars.shape[0]
+        self.n_batches = int(np.floor(self.n_samples) / self.batch_size)
+
+        # Get input and output variable indices
+        self.input_idxs = return_var_idxs(self.data_ds, self.input_vars_dict)
+        self.output_idxs = return_var_idxs(self.data_ds, self.output_vars_dict)
+        self.n_inputs, self.n_outputs = len(self.input_idxs), len(self.output_idxs)
+
+        # Initialize input and output normalizers/transformers
+        if self.input_transform is None:
+            self.input_transform = Normalizer()
+        elif type(self.input_transform) is tuple:
+            self.input_transform = InputNormalizer(
+                self.norm_ds,
+                self.input_vars_dict,
+                self.input_transform[0],
+                self.input_transform[1],
+                # var_cut_off,
+            )
+        else:
+            self.input_transform = (
+                self.input_transform  # Assume an initialized normalizer is passed
+            )
+
+        if self.output_transform is None:
+            self.output_transform = Normalizer()
+        elif type(self.output_transform) is dict:
+            self.output_transform = DictNormalizer(
+                self.norm_ds, self.output_vars_dict, self.output_transform
+            )
+        else:
+            self.output_transform = (
+                self.output_transform  # Assume an initialized normalizer is passed
+            )
+
+        # Now close the xarray file and load it as an h5 file instead
+        # This significantly speeds up the reading of the data...
+        if not self.xarray:
+            self.data_ds.close()
+            self.data_ds = h5py.File(self.data_fn, "r")
+        
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.data_ds.close()
+        try:
+            self.data_ds.close()
+        except:
+            pass
+#         try:
+#             self.norm_ds.close()
+#         except:
+#             pass
