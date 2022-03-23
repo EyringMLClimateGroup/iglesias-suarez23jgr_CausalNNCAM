a524cd3c61d2677df06a942a36d4e5fe52271d0c
Fernando Iglesias-Suarez <iglesias-suarez1@jwlogin21.juwels>
2022-03-23 16:40:03 +0100

    Implemented neural networks distributed-training via Horovod.

---
+++ neural_networks/data_generator_for_distributed_training.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+from .cbrain.data_generator_for_distributed_training import DataGenerator
+from .cbrain.utils import load_pickle
+
+
+def build_train_generator(input_vars_dict, output_vars_dict, setup):
+    out_scale_dict = load_pickle(
+        Path(setup.out_scale_dict_folder, setup.out_scale_dict_fn)
+    )
+    input_transform = (setup.input_sub, setup.input_div)
+
+    train_gen = DataGenerator(
+        data_fn=Path(setup.train_data_folder, setup.train_data_fn),
+        input_vars_dict=input_vars_dict,
+        output_vars_dict=output_vars_dict,
+        norm_fn=Path(setup.normalization_folder, setup.normalization_fn),
+        input_transform=input_transform,
+        output_transform=out_scale_dict,
+        batch_size=setup.batch_size,
+        shuffle=True,  # This feature doesn't seem to work
+    )
+    return train_gen
+
+
+def build_valid_generator(input_vars_dict, output_vars_dict, setup, nlat=64, nlon=128, test=False):
+    out_scale_dict = load_pickle(
+        Path(setup.out_scale_dict_folder, setup.out_scale_dict_fn)
+    )
+    input_transform = (setup.input_sub, setup.input_div)
+    if test:
+        data_fn = setup.test_data_folder
+        filenm  = setup.test_data_fn
+    else:
+        data_fn = setup.train_data_folder
+        filenm  = setup.valid_data_fn
+    
+    ngeo = nlat * nlon
+    valid_gen = DataGenerator(
+        data_fn=Path(data_fn, filenm),
+        input_vars_dict=input_vars_dict,
+        output_vars_dict=output_vars_dict,
+        norm_fn=Path(setup.normalization_folder, setup.normalization_fn),
+        input_transform=input_transform,
+        output_transform=out_scale_dict,
+        batch_size=ngeo,
+        shuffle=False,
+        # xarray=True,
+    )
+    return valid_gen
--- slurm_juwels-booster/juwels_booster_NN_Creation_distributed_training.sh
+++ slurm_juwels-booster/juwels_booster_NN_Creation_distributed_training.sh
@@ -2,25 +2,35 @@
 # juwels-booster batch job parameters
 # --------------------------------
 #SBATCH --account=cesmtst
-#SBATCH --job-name=p59TQ3
-#SBATCH --partition=booster
-####SBATCH --partition=develbooster
+#SBATCH --job-name=dist-test
+#####SBATCH --partition=booster
+#SBATCH --partition=develbooster
+#SBATCH --nodes=2
 #SBATCH --gres=gpu:4
-#SBATCH --nodes=1
-#SBATCH --output=LOG.p59TQ3-out_%j
-#SBATCH --error=LOG.p59TQ3-out_%j
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=12
+#SBATCH --output=LOG.dist-test-out_%j
+#SBATCH --error=LOG.dist-test-out_%j
 #SBATCH --mail-type=FAIL
-#SBATCH --time=08:00:00
+#SBATCH --time=02:00:00
 # --------------------------------
 # Run the script.
 # From the main repository directory:
 #    $> sbatch slurm_juwels/<script>.sh
 # --------------------------------
 
-logFile=`ls LOG.p59TQ3-out_*`
+logFile=`ls LOG.dist-test-out_*`
 
 # Scripts
-pyScript=NN_Creation
+pyScript=NN_Creation_distributed_training
+
+## Config files
+#
+# Test; e.g., job-name=test
+cfgFile=220316_distributed_training_test/cfg_NN_Creation_rasp_phq-tphystnd_12.yml
+#cfgFile=220316_distributed_training_test/cfg_NN_Creation_rasp_phq-tphystnd_29.yml
+
 #cfgFile=220224_threshold_optimization/thrs/cfg_NN_Creation_rasp_parcorr_thrs-0.05_tphystnd_29.yml
 #cfgFile=220224_threshold_optimization/thrs/cfg_NN_Creation_rasp_parcorr_thrs-0.05_phq_29.yml
 
@@ -30,15 +40,11 @@ pyScript=NN_Creation
 #cfgFile=220224_threshold_optimization/single/cfg_NN_Creation_rasp_tphystnd_28.yml
 #cfgFile=220224_threshold_optimization/single/cfg_NN_Creation_rasp_phq_28.yml
 
-# e.g., job-name=p59TQ21
-cfgFile=220308_rasp_etal_2018_optimized_threshold/quantile-0.59/cfg_NN_Creation_rasp_parcorr_pdf-0.59_phq-tphystnd_3.yml
+# e.g., job-name=p58TQ21
+#cfgFile=220308_rasp_etal_2018_optimized_threshold/quantile-0.58/cfg_NN_Creation_rasp_parcorr_pdf-0.58_phq-tphystnd_21.yml#
 
 # e.g., job-name=sglTQ29
 #cfgFile=220308_rasp_etal_2018_optimized_threshold/single/cfg_NN_Creation_rasp_phq-tphystnd_6.yml
-
-# Test; e.g., job-name=test
-#cfgFile=220310_distributed_epoch_test/cfg_testing.yml
-
 
 # Paths
 scriptPath=`pwd`
@@ -52,9 +58,10 @@ echo ""
 
 cat ./nn_config/$cfgFile > $logFile
 
-echo "Create NNs as per Rasp et al. (2018)."
+echo "Create NNs via parallel distributed training."
 source /p/home/jusers/iglesias-suarez1/juwels/.bash_profile
 conda activate causalnncam
+export PYTHONPATH="${PYTHONPATH}:/p/project/cesmtst/iglesias-suarez1/miniconda/envs/causalnncam/lib/python3.8/site-packages"
 
 if [ ! -f ${pyScript}.py ]; then
     echo "Convert jupyter notebook into python script"
@@ -62,7 +69,17 @@ if [ ! -f ${pyScript}.py ]; then
 fi
 echo ""
 
-python ${pyScript}.py -c ./nn_config/$cfgFile
+# Load the required modules
+module load Stages/2020
+module load GCC/9.3.0
+module load OpenMPI/4.1.0rc1
+module load TensorFlow/2.3.1-Python-3.8.5
+module load Horovod/0.20.3-Python-3.8.5
+
+# Make all GPUs visible per node
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+srun python -u ${pyScript}.py -c ./nn_config/$cfgFile
 
 # Clean-up
 rm ${pyScript}.py
